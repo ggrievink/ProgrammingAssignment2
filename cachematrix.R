7e7a4267f2e65702b14f74caf030cad2b69b1b56
Gerrit Jan Grievink <gerritjan@grievink.eu>
2015-01-18 14:52:52 +0100

    Version 1 of assignment week 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## 
+## The function make store a matrix and its inverse.
+## 
+makeCacheMatrix <- function(currentMatrix  = matrix()) {
+  
+  ## the calculated inverse matix
+  inverseMatrix <- NULL
+  
+  ## set the matrix and clear the inverse matrix.
+  set <- function(y) {
+      currentMatrix <<- y
+      inverseMatrix <<- NULL
+  }
+  
+  ## get the matrix
+  get <- function() { currentMatrix }
+  
+  ## get the inverse matrix.
+  getInverseMatrix <-  function() { inverseMatrix }
+  
+  ## set the solved  matrix
+  setInverseMatrix <-  function(y) { inverseMatrix <<- y }
+  
+  ## list with functions.
+  list(set = set, 
+       get = get,
+       getInverseMatrix = getInverseMatrix,
+       setInverseMatrix = setInverseMatrix)
 }
 
-
-## Write a short comment describing this function
-
+## Before it calculates the inverse the function checks 
+## if the inverse is already pressent in the catch.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+    ## get the current inver matrix
+    invMatrix <- x$getInverseMatrix()
+    
+    ## test if the inverse matrix is NULL.
+    ## when NULL the inverse matrix must be solved.
+    if(is.null(invMatrix)){
+        ## no calculated value pressent, calculate the inverse matrix,
+        ## message("calculate inv matrix")
+        ## get the matrix and calculate inverse
+        invMatrix <- solve( x$get(), ... )
+        ## calulate and set inverse.
+        x$setInverseMatrix(invMatrix )
+    }
+    ## get the inverse matrix
+    return(invMatrix)
 }
